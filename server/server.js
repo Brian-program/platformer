2727f10611e10eace098502c3b75bb08ad04fed3
Brian <brian@lev-04215.apn.wlan.private.upenn.edu>
2023-04-30 15:54:30 -0400

    neaten server

--- server/server.js
+++ server/server.js
@@ -8,16 +8,13 @@ app.use(cors({
   origin: '*',
 }));
 
-// We use express to define our various API endpoints and
-// provide their handlers that we implemented in routes.js
+//get methods
 app.get('/movies/:titleId', routes.movies);
-// app.get('/random', routes.random);
 app.get('/friendlist/:userId', routes.friendlist);
 app.get('/watchlist/:userId', routes.watchlist);
 app.get('/top_movies', routes.top_movies);
 app.get('/simple_search', routes.simple_search);
 app.get('/advanced_search', routes.advanced_search);
-
 app.get('/top_DisneyPlus', routes.top_DisneyPlus);
 app.get('/top_PrimeVideo', routes.top_PrimeVideo);
 app.get('/top_netflix', routes.top_netflix);
@@ -37,7 +34,7 @@ app.post('/add_user', routes.add_user);
 app.delete('/remove_friendlist', routes.remove_friendlist);
 app.delete('/remove_watchlist', routes.remove_watchlist);
 
-
+//format
 app.listen(config.server_port, () => {
   console.log(`Server running at http://${config.server_host}:${config.server_port}/`)
 });
